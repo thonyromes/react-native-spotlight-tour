636bcb3c097e25c60b56ebb2bf304379a2339a24
Romes <thonyromes@gmail.com>
2024-09-13 13:30:26 +0100

    fix: overlay handler object

--- package/src/lib/components/tour-overlay/TourOverlay.component.tsx
+++ package/src/lib/components/tour-overlay/TourOverlay.component.tsx
@@ -61,208 +61,240 @@ interface TourOverlayProps extends ToOptional<TooltipProps> {
   tourStep: TourStep;
 }
 
-export const TourOverlay = forwardRef<TourOverlayRef, TourOverlayProps>((props, ref) => {
-  const {
-    backdropOpacity,
-    color,
-    current,
-    motion,
-    nativeDriver,
-    onBackdropPress,
-    padding,
-    shape,
-    spot,
-    tourStep,
-    ...tooltipProps
-  } = props;
+export const TourOverlay = forwardRef<TourOverlayRef, TourOverlayProps>(
+  (props, ref) => {
+    const {
+      backdropOpacity,
+      color,
+      current,
+      motion,
+      nativeDriver,
+      onBackdropPress,
+      padding,
+      shape,
+      spot,
+      tourStep,
+      ...tooltipProps
+    } = props;
 
-  const { goTo, next, previous, start, steps, stop } = useContext(SpotlightTourContext);
+    const { goTo, next, previous, start, steps, stop, changeSpot } =
+      useContext(SpotlightTourContext);
 
-  const arrowRef = useRef<View>(null);
+    const arrowRef = useRef<View>(null);
 
-  const floating = useMemo((): TooltipProps => ({
-    arrow: tourStep.arrow ?? tooltipProps.arrow,
-    flip: tourStep.flip ?? tooltipProps.flip,
-    offset: tourStep.offset ?? tooltipProps.offset,
-    placement: tourStep.placement ?? tooltipProps.placement,
-    shift: tourStep.shift ?? tooltipProps.shift,
-  }), [tooltipProps, tourStep.arrow, tourStep.flip, tourStep.offset, tourStep.placement, tourStep.shift]);
+    const floating = useMemo(
+      (): TooltipProps => ({
+        arrow: tourStep.arrow ?? tooltipProps.arrow,
+        flip: tourStep.flip ?? tooltipProps.flip,
+        offset: tourStep.offset ?? tooltipProps.offset,
+        placement: tourStep.placement ?? tooltipProps.placement,
+        shift: tourStep.shift ?? tooltipProps.shift,
+      }),
+      [
+        tooltipProps,
+        tourStep.arrow,
+        tourStep.flip,
+        tourStep.offset,
+        tourStep.placement,
+        tourStep.shift,
+      ],
+    );
 
-  const floatingOptions = useMemo(() => {
-    return makeFloatingOptions(arrowRef, floating);
-  }, [floating]);
+    const floatingOptions = useMemo(() => {
+      return makeFloatingOptions(arrowRef, floating);
+    }, [floating]);
 
-  const { refs, floatingStyles, middlewareData, placement } = useFloating(floatingOptions);
+    const { refs, floatingStyles, middlewareData, placement } =
+      useFloating(floatingOptions);
 
-  const tooltipOpacity = useRef(new Animated.Value(0));
+    const tooltipOpacity = useRef(new Animated.Value(0));
 
-  const stepMotion = useMemo((): Motion => {
-    return tourStep.motion ?? motion;
-  }, [tourStep, motion]);
+    const stepMotion = useMemo((): Motion => {
+      return tourStep.motion ?? motion;
+    }, [tourStep, motion]);
 
-  const stepShape = useMemo((): Shape => {
-    return tourStep.shape ?? shape;
-  }, [tourStep, shape]);
+    const stepShape = useMemo((): Shape => {
+      return tourStep.shape ?? shape;
+    }, [tourStep, shape]);
 
-  const useNativeDriver = useMemo(() => {
-    const driverConfig: OSConfig<boolean> = typeof nativeDriver === "boolean"
-      ? { android: nativeDriver, ios: nativeDriver, web: nativeDriver }
-      : nativeDriver;
+    const useNativeDriver = useMemo(() => {
+      const driverConfig: OSConfig<boolean> =
+        typeof nativeDriver === "boolean"
+          ? { android: nativeDriver, ios: nativeDriver, web: nativeDriver }
+          : nativeDriver;
 
-    return Platform.select({
-      android: driverConfig.android,
-      default: false,
-      ios: driverConfig.ios,
-      web: false,
-    });
-  }, [nativeDriver]);
-
-  const ShapeMask = useMemo(<P extends ShapeProps>(): ComponentType<P> => {
-    switch (stepShape) {
-      case "circle": return CircleShape;
-      case "rectangle": return RectShape;
-    }
-  }, [stepShape]);
-
-  const handleBackdropPress = useCallback((): void => {
-    const handler = tourStep.onBackdropPress ?? onBackdropPress;
-
-    if (handler !== undefined && current !== undefined) {
-      switch (handler) {
-        case "continue":
-          return next();
-
-        case "stop":
-          return stop();
-
-        default:
-          return handler({ current, goTo, next, previous, start, stop });
-      }
-    }
-  }, [tourStep, onBackdropPress, current, goTo, next, previous, start, stop]);
-
-  useEffect(() => {
-    const { height, width } = spot;
-
-    if ([height, width].every(value => value > 0)) {
-      Animated.timing(tooltipOpacity.current, {
-        delay: 400,
-        duration: 400,
-        toValue: 1,
-        useNativeDriver,
-      })
-      .start();
-    }
-  }, [spot, useNativeDriver]);
-
-  useImperativeHandle<TourOverlayRef, TourOverlayRef>(ref, () => ({
-    hideTooltip: () => {
-      return new Promise(resolve => {
-        if (current !== undefined) {
-          Animated.timing(tooltipOpacity.current, {
-            duration: 400,
-            toValue: 0,
-            useNativeDriver,
-          })
-          .start(resolve);
-        } else {
-          resolve({ finished: true });
-        }
+      return Platform.select({
+        android: driverConfig.android,
+        default: false,
+        ios: driverConfig.ios,
+        web: false,
       });
-    },
-  }), [current, useNativeDriver]);
+    }, [nativeDriver]);
 
-  return (
-    <Modal
-      animationType="fade"
-      presentationStyle="overFullScreen"
-      transparent={true}
-      visible={current !== undefined}
-    >
-      <OverlayView testID="Overlay View">
-        <Svg
-          testID="Spot Svg"
-          height="100%"
-          width="100%"
-          viewBox={`0 0 ${vwDP(100)} ${vhDP(100)}`}
-          onPress={handleBackdropPress}
-          shouldRasterizeIOS={true}
-          renderToHardwareTextureAndroid={true}
-        >
-          <Defs>
-            <Mask id="mask" x={0} y={0} height="100%" width="100%">
-              <Rect height="100%" width="100%" fill="#fff" />
-              <ShapeMask
-                spot={spot}
-                setReference={refs.setReference}
-                motion={stepMotion}
-                padding={padding}
-                useNativeDriver={useNativeDriver}
-              />
-            </Mask>
-          </Defs>
-          <Rect
+    const ShapeMask = useMemo(<P extends ShapeProps>(): ComponentType<P> => {
+      switch (stepShape) {
+        case "circle":
+          return CircleShape;
+        case "rectangle":
+          return RectShape;
+      }
+    }, [stepShape]);
+
+    const handleBackdropPress = useCallback((): void => {
+      const handler = tourStep.onBackdropPress ?? onBackdropPress;
+
+      if (handler !== undefined && current !== undefined) {
+        switch (handler) {
+          case "continue":
+            return next();
+
+          case "stop":
+            return stop();
+
+          default:
+            return handler({
+              current,
+              goTo,
+              next,
+              previous,
+              start,
+              stop,
+              spot,
+              steps,
+              changeSpot,
+            });
+        }
+      }
+    }, [tourStep, onBackdropPress, current, goTo, next, previous, start, stop]);
+
+    useEffect(() => {
+      const { height, width } = spot;
+
+      if ([height, width].every((value) => value > 0)) {
+        Animated.timing(tooltipOpacity.current, {
+          delay: 400,
+          duration: 400,
+          toValue: 1,
+          useNativeDriver,
+        }).start();
+      }
+    }, [spot, useNativeDriver]);
+
+    useImperativeHandle<TourOverlayRef, TourOverlayRef>(
+      ref,
+      () => ({
+        hideTooltip: () => {
+          return new Promise((resolve) => {
+            if (current !== undefined) {
+              Animated.timing(tooltipOpacity.current, {
+                duration: 400,
+                toValue: 0,
+                useNativeDriver,
+              }).start(resolve);
+            } else {
+              resolve({ finished: true });
+            }
+          });
+        },
+      }),
+      [current, useNativeDriver],
+    );
+
+    return (
+      <Modal
+        animationType="fade"
+        presentationStyle="overFullScreen"
+        transparent={true}
+        visible={current !== undefined}
+      >
+        <OverlayView testID="Overlay View">
+          <Svg
+            testID="Spot Svg"
             height="100%"
             width="100%"
-            fill={color}
-            mask="url(#mask)"
-            opacity={backdropOpacity}
-          />
-        </Svg>
-
-        {current !== undefined && (
-          <Animated.View
-            ref={refs.setFloating}
-            testID="Tooltip View"
-            style={{ ...floatingStyles, opacity: tooltipOpacity.current }}
+            viewBox={`0 0 ${vwDP(100)} ${vhDP(100)}`}
+            onPress={handleBackdropPress}
+            shouldRasterizeIOS={true}
+            renderToHardwareTextureAndroid={true}
           >
-            <tourStep.render
-              current={current}
-              isFirst={current === 0}
-              isLast={current === steps.length - 1}
-              next={next}
-              previous={previous}
-              stop={stop}
-              goTo={goTo}
+            <Defs>
+              <Mask id="mask" x={0} y={0} height="100%" width="100%">
+                <Rect height="100%" width="100%" fill="#fff" />
+                <ShapeMask
+                  spot={spot}
+                  setReference={refs.setReference}
+                  motion={stepMotion}
+                  padding={padding}
+                  useNativeDriver={useNativeDriver}
+                />
+              </Mask>
+            </Defs>
+            <Rect
+              height="100%"
+              width="100%"
+              fill={color}
+              mask="url(#mask)"
+              opacity={backdropOpacity}
             />
-            {floating.arrow !== false && (
-              <TooltipArrow
-                ref={arrowRef}
-                placement={placement}
-                data={middlewareData.arrow}
-                arrow={floating.arrow === true ? undefined : floating.arrow}
-              />
-            )}
-          </Animated.View>
-        )}
-      </OverlayView>
-    </Modal>
-  );
-});
+          </Svg>
 
-function makeFloatingOptions(arrowRef: RefObject<View>, props: Optional<TooltipProps>): UseFloatingOptions {
-  const arrowOption = typeof props?.arrow === "boolean"
-    ? DEFAULT_ARROW
-    : props?.arrow;
-  const { size } = typeof arrowOption === "number"
-    ? { ...DEFAULT_ARROW, size: arrowOption }
-    : { ...DEFAULT_ARROW, ...arrowOption };
+          {current !== undefined && (
+            <Animated.View
+              ref={refs.setFloating}
+              testID="Tooltip View"
+              style={{ ...floatingStyles, opacity: tooltipOpacity.current }}
+            >
+              <tourStep.render
+                current={current}
+                isFirst={current === 0}
+                isLast={current === steps.length - 1}
+                next={next}
+                previous={previous}
+                stop={stop}
+                goTo={goTo}
+              />
+              {floating.arrow !== false && (
+                <TooltipArrow
+                  ref={arrowRef}
+                  placement={placement}
+                  data={middlewareData.arrow}
+                  arrow={floating.arrow === true ? undefined : floating.arrow}
+                />
+              )}
+            </Animated.View>
+          )}
+        </OverlayView>
+      </Modal>
+    );
+  },
+);
+
+function makeFloatingOptions(
+  arrowRef: RefObject<View>,
+  props: Optional<TooltipProps>,
+): UseFloatingOptions {
+  const arrowOption =
+    typeof props?.arrow === "boolean" ? DEFAULT_ARROW : props?.arrow;
+  const { size } =
+    typeof arrowOption === "number"
+      ? { ...DEFAULT_ARROW, size: arrowOption }
+      : { ...DEFAULT_ARROW, ...arrowOption };
   const baseOffset = props?.offset || 4;
-  const offsetValue = props?.arrow !== false
-    ? (Math.sqrt(2 * size ** 2) / 2) + baseOffset
-    : baseOffset;
-  const arrowMw = props?.arrow !== false
-    ? arrow({ element: arrowRef })
-    : undefined;
-  const flipMw = props?.flip !== false
-    ? flip(props?.flip === true ? undefined : props?.flip)
-    : undefined;
-  const offsetMw = props?.offset !== 0
-    ? offset(offsetValue)
-    : undefined;
-  const shiftMw = props?.shift !== false
-    ? shift(typeof props?.shift === "object" ? props.shift : { padding: 8 })
-    : undefined;
+  const offsetValue =
+    props?.arrow !== false
+      ? Math.sqrt(2 * size ** 2) / 2 + baseOffset
+      : baseOffset;
+  const arrowMw =
+    props?.arrow !== false ? arrow({ element: arrowRef }) : undefined;
+  const flipMw =
+    props?.flip !== false
+      ? flip(props?.flip === true ? undefined : props?.flip)
+      : undefined;
+  const offsetMw = props?.offset !== 0 ? offset(offsetValue) : undefined;
+  const shiftMw =
+    props?.shift !== false
+      ? shift(typeof props?.shift === "object" ? props.shift : { padding: 8 })
+      : undefined;
 
   return {
     middleware: [flipMw, offsetMw, shiftMw, arrowMw].filter(Boolean),
