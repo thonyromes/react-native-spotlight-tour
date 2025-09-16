d7c5d8a96f0e1c2b8f41af8077c82cc195501301
Romes <thonyromes@gmail.com>
2025-09-16 19:06:14 +0100

    revert: responsive

--- package/src/helpers/responsive.ts
+++ package/src/helpers/responsive.ts
@@ -1,39 +1,9 @@
 import { responsiveScreenHeight, responsiveScreenWidth } from "react-native-responsive-dimensions";
 
-import type { SafeAreaInsets } from "../lib/SpotlightTour.context";
-
-export function vw(percentage: number, safeAreaInsets?: SafeAreaInsets): number {
-  const screenWidth = responsiveScreenWidth(percentage);
-  
-  if (safeAreaInsets) {
-    const availableWidth = responsiveScreenWidth(100) - safeAreaInsets.left - safeAreaInsets.right;
-    return (availableWidth * percentage) / 100;
-  }
-  
-  return screenWidth;
+export function vw(percentage: number): number {
+  return responsiveScreenWidth(percentage);
 }
 
-export function vh(percentage: number, safeAreaInsets?: SafeAreaInsets): number {
-  const screenHeight = responsiveScreenHeight(percentage);
-  
-  if (safeAreaInsets) {
-    const availableHeight = responsiveScreenHeight(100) - safeAreaInsets.top - safeAreaInsets.bottom;
-    return (availableHeight * percentage) / 100;
-  }
-  
-  return screenHeight;
-}
-
-/**
- * Get safe area adjusted viewport width
- */
-export function safeVw(percentage: number, safeAreaInsets: SafeAreaInsets): number {
-  return vw(percentage, safeAreaInsets);
-}
-
-/**
- * Get safe area adjusted viewport height
- */
-export function safeVh(percentage: number, safeAreaInsets: SafeAreaInsets): number {
-  return vh(percentage, safeAreaInsets);
+export function vh(percentage: number): number {
+  return responsiveScreenHeight(percentage);
 }
